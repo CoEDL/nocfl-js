cc3beb20034a57e0d11473cba2e5c1c1f5c3ae93
Marco La Rosa <m@lr.id.au>
2022-08-19 09:54:22 +1000

    cleanup

--- src/store.js
+++ src/store.js
@@ -59,7 +59,7 @@ export class Store {
         if (!isString(className)) {
             throw new Error(`The 'className' must be a string`);
         }
-        if (!isString(domain) && !isUndefined(domain)) {
+        if (!isString(domain)) {
             throw new Error(`The 'domain' must be a string`);
         }
 
