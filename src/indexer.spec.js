6986cc584766e2917b8147baba1cfb5e9cf4f11f
Marco La Rosa <m@lr.id.au>
2022-08-19 13:36:27 +1000

    implement indexer functionality

--- src/indexer.spec.js
+++ src/indexer.spec.js
@@ -17,16 +17,16 @@ const credentials = {
 };
 const bucket = new Bucket(credentials);
 
-describe(`Test creating index files - 1 domain`, () => {
+describe(`Test managing index files - 1 domain`, () => {
     const domain = chance.domain();
-    beforeAll(async () => {
+    beforeEach(async () => {
         await setupTestData({
             domain,
             credentials,
             count: 3,
         });
     });
-    afterAll(async () => {
+    afterEach(async () => {
         await bucket.removeObjects({ prefix: domain });
     });
     it("Should find all items in the repository and create index files to them", async () => {
@@ -36,33 +36,110 @@ describe(`Test creating index files - 1 domain`, () => {
             expect(await bucket.pathExists({ path: file })).toBe(true);
         }
     });
-});
+    it(`should be able to PUT a reference into an index file - index file doesn't exist yet`, async () => {
+        const indexer = new Indexer({ credentials });
+        const id = chance.word();
+        const className = "collection";
+        await indexer.patchIndex({
+            action: "PUT",
+            domain,
+            className,
+            id,
+        });
 
-describe(`Test creating index files - 3 domains`, () => {
-    const domains = [chance.domain(), chance.domain(), chance.domain()];
-    beforeAll(async () => {
-        for (let domain of domains) {
-            await setupTestData({
-                domain,
-                credentials,
-                count: 3,
-            });
-        }
+        let indexFileName = `${domain}/indices/${className}/${id.slice(0, 1).toLowerCase()}.json`;
+        let indexFile = await bucket.readJSON({ target: indexFileName });
+        expect(indexFile.length).toEqual(1);
+        expect(indexFile[0].id).toEqual(id);
+        expect(indexFile[0].className).toEqual(className);
     });
-    afterAll(async () => {
-        for (let domain of domains) {
-            await bucket.removeObjects({ prefix: domain });
-        }
+    it(`should be able to PUT a reference into an index file - index file exists`, async () => {
+        const indexer = new Indexer({ credentials });
+
+        const store = new Store({
+            domain,
+            className: "collection",
+            id: "identifier1",
+            credentials,
+        });
+        await store.createItem();
+        await indexer.createIndices({});
+
+        let indexFileName = `${domain}/indices/collection/i.json`;
+        let indexFileBefore = await bucket.readJSON({ target: indexFileName });
+        expect(indexFileBefore.length).toBeGreaterThanOrEqual(1);
+
+        await indexer.patchIndex({
+            action: "PUT",
+            domain,
+            className: "collection",
+            id: "identifier2",
+        });
+
+        indexFileName = `${domain}/indices/collection/i.json`;
+        let indexFileAfter = await bucket.readJSON({ target: indexFileName });
+        expect(indexFileAfter.length).toBeGreaterThanOrEqual(2);
     });
-    it("Should find all items in the repository and create index files to them", async () => {
+    it(`should be able to DELETE a reference from an index file - index file exists`, async () => {
+        const indexer = new Indexer({ credentials });
+
+        const store = new Store({
+            domain,
+            className: "collection",
+            id: "identifier1",
+            credentials,
+        });
+        await store.createItem();
+        await indexer.createIndices({});
+
+        let indexFileName = `${domain}/indices/collection/i.json`;
+        let indexFileBefore = await bucket.readJSON({ target: indexFileName });
+        expect(indexFileBefore.length).toBeGreaterThanOrEqual(1);
+
+        await indexer.patchIndex({
+            action: "DELETE",
+            domain,
+            className: "collection",
+            id: "identifier1",
+        });
+
+        indexFileName = `${domain}/indices/collection/i.json`;
+        let indexFileAfter = await bucket.readJSON({ target: indexFileName });
+        expect(indexFileAfter.length).toEqual(indexFileBefore.length - 1);
+    });
+    it(`should be able to retrieve a specified index file`, async () => {
+        const indexer = new Indexer({ credentials });
+
+        const store = new Store({
+            domain,
+            className: "collection",
+            id: "identifier1",
+            credentials,
+        });
+        await store.createItem();
+        await indexer.createIndices({});
+
+        let indexFile = await indexer.getIndex({ domain, className: "collection", prefix: "i" });
+        expect(indexFile.length).toBeGreaterThanOrEqual(1);
+        indexFile = await indexer.getIndex({ domain, className: "collection", file: "i.json" });
+        expect(indexFile.length).toBeGreaterThanOrEqual(1);
+    });
+    it(`should be able to list all domain indexes`, async () => {
         const indexer = new Indexer({ credentials });
         let indexFiles = await indexer.createIndices({});
-        let domains = uniq(indexFiles.map((file) => file.split("/").shift()));
-        expect(domains.length).toEqual(3);
 
-        indexFiles = await indexer.createIndices({ domain: domains[1] });
-        domains = uniq(indexFiles.map((file) => file.split("/").shift()));
-        expect(domains.length).toEqual(1);
+        let indexFilesList = await indexer.listIndices({
+            domain,
+        });
+        expect(indexFilesList.length).toEqual(indexFiles.length);
+
+        indexFilesList = await indexer.listIndices({
+            domain,
+            className: "collection",
+        });
+        expect(indexFilesList.length).toEqual(
+            indexFiles.filter((i) => i.match("collection")).length
+        );
     });
 });
 
